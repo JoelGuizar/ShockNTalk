cc891261cbf180469ba2b423b59c938bd1298292
Joel Guizar <joelguizar@gmail.com>
2017-09-14 00:45:22 -0700

    socket.io initialized

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
+const socketIO = require('socket.io');
 
 console.log(__dirname + '/../public');
 console.log();
@@ -8,8 +10,12 @@ console.log();
 
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public')
+const server = http.createServer(app)
+
+
 app.use(express.static(publicPath)) //static middleware for public folder
 
+//need to make socket.io work with the create HTTP module express is using behind the scenes
 app.listen(PORT, () => {
   console.log(`Running on PORT ${PORT}.`);
 })
