c5ab425d6da4ee964c4af51f917133a5fffacfc6
Joel Guizar <joelguizar@gmail.com>
2017-09-14 15:41:25 -0700

    frontend html initialized

--- server/server.js
+++ server/server.js
@@ -10,12 +10,18 @@ console.log();
 
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public')
-const server = http.createServer(app)
+const server = http.createServer(app) //needs the og method
+const io = socketIO(server); //calls it on the http server, we get back the websocket server
 
 
 app.use(express.static(publicPath)) //static middleware for public folder
 
+//this is an io event emitter that is listening for an event
+io.on('connection', (socket) => {
+  console.log('new user connected');
+}) // the arg is the particular socket instead of all the users
+
 //need to make socket.io work with the create HTTP module express is using behind the scenes
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Running on PORT ${PORT}.`);
 })
